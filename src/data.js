26a1d8f12b40113e9e1dc7f533afd3fa94cd3894
Alex Davies <alxdavies@googlemail.com>
2023-05-10 10:14:26 +0100

    copy changed in data file

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const artworks = [
   {
     title: "Coronation",
     artist: "Unknown artist",
-    media: ["found objects", "sculpture"],
+    media: ["cleaning sign, porcelain sink"],
     description: "This unique sculpture, crafted to commemorate the coronation of King Charles, repurposes a humble cleaning sign, transforming it into a symbol of regal significance. The juxtaposition of the sign's utilitarian purpose with the grandeur of royalty creates a powerful visual narrative, symbolising the potential that lies within even the most ordinary objects.",
     image: "./coronation.jpg",
     year: "2023",
@@ -10,8 +10,8 @@ export const artworks = [
     {
     title: "Future Thermostat",
     artist: "Unknown artist",
-    media: ["print on plastic backing",],
-    description: '"Future Thermostat" is a captivating artwork crafted entirely from a plastic socket cover, meticulously transformed into a thought-provoking sculptural installation. Standing at the intersection of functionality and aesthetics, the artwork symbolizes the integration of technology into our daily lives, particularly in relation to the control and regulation of our living environments.',
+    media: ["printed label on plastic",],
+    description: '"Future Thermostat" is a captivating artwork crafted from a plastic socket cover, meticulously transformed into a thought-provoking sculptural installation. Standing at the intersection of functionality and aesthetics, the artwork questions the role technology plays in our daily lives. Presented with a promise of future control, regulation and heat, yet with no timescale to fulfil this pledge, the viewer is left stranded in a perpetual state of anticipation and ultimately confusion. ',
     image: "./future-thermostat.jpg",
     year: "2023",
   },
