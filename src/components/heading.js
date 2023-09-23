7c5db78d85b1ffa14a4d9531757d1df9eb9646e9
Alex Davies <alxdavies@googlemail.com>
2023-09-23 22:55:20 +0100

    new artwork added

--- src/components/heading.js
+++ src/components/heading.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Heading() {
   return(
-    <div className="navbar-content">
+    <div className="title-content">
       <br />
       <hr />
       <br />
-      <div className="navbar-text">
+      <div className="title-text">
         <h1>Serendipitous Masterpieces: Embracing the Beauty of Art Created by Accident</h1>
         <p>Welcome to "Serendipitous Masterpieces," a collection of art that celebrates the unexpected beauty that arises from accidental creative practices. This captivating collection showcases a diverse range of works that have emerged from fortuitous accidents, offering a unique glimpse into the extraordinary creative potential that can emerge from unexpected circumstances.</p>
       </div>
--- src/data.js
+++ src/data.js
@@ -15,4 +15,12 @@ export const artworks = [
     image: "./future-thermostat.jpg",
     year: "2023",
   },
+    {
+    title: "Transcendent Nostalgia",
+    artist: "Unknown artist",
+    media: ["parcel shelf, floorboards and table",],
+    description: '"Transcendent Nostalgia" is a site-specific sculpture that boldly reimagines the discarded remnants of everyday objects into a thought-provoking, immersive structural installation. At the head of this structure stands an old car parcel shelf, elevated on a pedestal, serving as both the centerpiece and a symbol of forgotten journeys and stories. The parcel shelf, once a functional part of a car, is now repurposed to evoke a sense of suspended motion and transition. Its void-like form creates an immediate focal point, drawing viewers into the narrative. Propping up the parcel shelf are tongue and groove floorboards, carefully arranged to create a cascading mid-section that feels at once both solid and precarious. Their aged appearance hints at the passage of time and the accumulation of memories, inviting viewers to contemplate their own life journey."' ,
+    image: "./transcendent-nostalgia.jpg",
+    year: "2023",
+  },
 ]
