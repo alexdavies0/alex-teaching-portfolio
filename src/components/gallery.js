c5ea9ccd5444b770869eb9cff8d37f73833a3dfb
Alex Davies <alxdavies@googlemail.com>
2024-01-30 22:21:15 +0000

    new site

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -3,38 +3,43 @@ import { artworks } from "../data";
 
 export default function Gallery() {
   return (
-      <div>
-        {artworks.map((artwork) => (
-        <div className="center-screen">
-          <div className="project-card">
-            <img className="project-image"
-            alt={artwork.title}
-            src={artwork.image}
-            />
-          <div className="project-text">
-            <h2>
-              <strong>{artwork.title}</strong>
+      <div class="container mx-auto">
+        <div class="grid grid-cols-3 gap-4">
+          <div>
+            {artworks.map((artwork) => (
+            <div className="center-screen">
+              <div className="project-card">
+                <img className="project-image"
+                alt={artwork.title}
+                src={artwork.image_url}
+                />
+              <div className="project-text">
+                <h2>
+                  <strong>{artwork.title}</strong>
+                  <br />
+                  <em>{artwork.media}</em>
+                  <br />
+                  {artwork.ks}
+                </h2>
+              </div>
+
+
+              <p>
+                {artwork.description}
+              </p>
               <br />
-              <em>{artwork.artist}</em> ({artwork.year})
               <br />
-              {artwork.media[0]}
-            </h2>
+              <hr />
+              <br />
+              <br />
+
+            </div>
+            </div>
+
+            ))}
           </div>
-
-
-          <p>
-            {artwork.description}
-          </p>
-          <br />
-          <br />
-          <hr />
-          <br />
-          <br />
-
-        </div>
         </div>
 
-        ))}
       </div>
 
   );
--- src/components/heading.js
+++ src/components/heading.js
@@ -7,8 +7,8 @@ export default function Heading() {
       <hr />
       <br />
       <div className="title-text">
-        <h1>Serendipitous Masterpieces: Embracing the Beauty of Art Created by Accident</h1>
-        <p>Welcome to "Serendipitous Masterpieces," a collection of art that celebrates the unexpected beauty that arises from accidental creative practices. This captivating collection showcases a diverse range of works that have emerged from fortuitous accidents, offering a unique glimpse into the extraordinary creative potential that can emerge from unexpected circumstances.</p>
+        <h1>Alex Davies Portfolio</h1>
+        <p>Artworks produced by students in Alex's KS3 and 4 Art classes using a range of materials and processes.</p>
       </div>
       <br />
       <hr />
